fb4e8573421a6313ffe6c9b5c36499ed18182eff
Abhimanyu <abhimanyuahluwalia19@gmail.com>
2019-02-23 20:43:42 -0700

    part (b) done

--- cse360/src/cse360assign2/Calculator.java
+++ cse360/src/cse360assign2/Calculator.java
@@ -8,27 +8,38 @@ public class Calculator {
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal () 
+	{
+		return total;
 	}
 	
-	public void add (int value) {
+	public void add (int value) 
+	{
+		total = total + value;
+	}
+	
+	public void subtract (int value) 
+	{
+		total = total - value;
+	}
+	
+	public void multiply (int value) 
+	{
+		total = total*value;
+	}
+	
+	public void divide (int value) 
+	{
+		if(value == 0)
+		{
+			total = 0;
+		}
 		
+		total = total/value;
 	}
 	
-	public void subtract (int value) {
-		
-	}
-	
-	public void multiply (int value) {
-		
-	}
-	
-	public void divide (int value) {
-		
-	}
-	
-	public String getHistory () {
+	public String getHistory () 
+	{
 		return "";
 	}
 }
