0b220f141c190cd6d01c1d0b3e23504d12a0b2fc
Abhimanyu <abhimanyuahluwalia19@gmail.com>
2019-02-25 01:08:46 -0700

    part (b) done

--- cse360/src/cse360assign2/Calculator.java
+++ cse360/src/cse360assign2/Calculator.java
@@ -6,7 +6,7 @@
  *              of different methods namely getTotal, add, subtract,
  *              multiply, divide and getHistory including a constructor.
  */
- 
+
 package cse360assign2;
 
 /**
@@ -27,56 +27,63 @@ public class Calculator
 	}
 	
 	/**
-	 * This method returns 0.
-	 * @return zero.
+	 * This method returns the total when called.
+	 * @return the total at the moment when called.
 	 */
 	public int getTotal () 
 	{
-		return 0;
+		return total;
 	}
 	
 	/**
-	 * This method accepts a parameter called value.
-	 * @param value
+	 * This method adds the parameter passed to the variable total.
+	 * @param value   the value passed by the user
 	 */
 	public void add (int value) 
 	{
-		
+		total = total + value;
 	}
 	
 	/**
-	 * This method accepts a parameter called value.
-	 * @param value
+	 * This method subtracts the parameter 'value' from total.
+	 * @param value   the value passed by the user
 	 */
 	public void subtract (int value) 
 	{
-		
+		total = total - value;
 	}
 	
 	/**
-	 * This method accepts a parameter called value.
-	 * @param value
+	 * This method multiply the parameter 'value' to total.
+	 * @param value   the value passed by the user
 	 */
 	public void multiply (int value) 
 	{
-		
+		total = total * value;
 	}
 	
 	/**
-	 * This method accepts a parameter called value.
-	 * @param value
+	 * This method equals total to zero if the parameter passed
+	 * is equal to zero. Else, it divides the total to the parameter
+	 * 'value'.
+	 * @param value   the value passed by the user
 	 */
 	public void divide (int value) 
 	{
+		if(value == 0)
+		{
+			total = 0;
+		}
 		
+		total = total/value;
 	}
 	
 	/**
-	 * This method returns an empty string
-	 * @param an empty string
+	 * This method returns an empty string.
+	 * @return   an empty string
 	 */
 	public String getHistory () 
 	{
-		return "";
+		return " ";
 	}
 }
