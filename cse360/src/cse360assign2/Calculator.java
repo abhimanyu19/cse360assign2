876efe5b7e091a81946eeb09d1e89af583217e47
Abhimanyu <abhimanyuahluwalia19@gmail.com>
2019-02-25 01:04:04 -0700

    Part (a) done

--- cse360/src/cse360assign2/Calculator.java
+++ cse360/src/cse360assign2/Calculator.java
@@ -1,43 +1,80 @@
+/**
+ * Author Name: ABHIMANYU AHLUWALIA
+ * Class ID: 304
+ * Assignment #2
+ * Description: There is a class called Calculator which consists
+ *              of different methods namely getTotal, add, subtract,
+ *              multiply, divide and getHistory including a constructor.
+ */
+ 
 package cse360assign2;
 
-public class Calculator {
-
+/**
+ * A class Calculator consists of different methods and each method
+ * performs certain operations.
+ */
+public class Calculator 
+{
 	private int total;
 	
-	public Calculator () {
+	/**
+	 * A constructor of Calculator class which initializes total variable
+	 * equals to zero.
+	 */
+	public Calculator () 
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * This method returns 0.
+	 * @return zero.
+	 */
 	public int getTotal () 
 	{
-		return total;
+		return 0;
 	}
 	
+	/**
+	 * This method accepts a parameter called value.
+	 * @param value
+	 */
 	public void add (int value) 
 	{
-		total = total + value;
+		
 	}
 	
+	/**
+	 * This method accepts a parameter called value.
+	 * @param value
+	 */
 	public void subtract (int value) 
 	{
-		total = total - value;
+		
 	}
 	
+	/**
+	 * This method accepts a parameter called value.
+	 * @param value
+	 */
 	public void multiply (int value) 
 	{
-		total = total*value;
+		
 	}
 	
+	/**
+	 * This method accepts a parameter called value.
+	 * @param value
+	 */
 	public void divide (int value) 
 	{
-		if(value == 0)
-		{
-			total = 0;
-		}
 		
-		total = total/value;
 	}
 	
+	/**
+	 * This method returns an empty string
+	 * @param an empty string
+	 */
 	public String getHistory () 
 	{
 		return "";
